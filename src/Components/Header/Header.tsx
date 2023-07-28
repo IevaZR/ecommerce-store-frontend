ba370d8126089acc5fd55da9b8c7677d8b1b5161
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-07-28 16:54:13 +0300

    header started

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,28 @@
-import "./Header.css"
+import "./Header.css";
+// @ts-ignore
+import Logo from "./../../Assets/logo.png";
+// @ts-ignore
+import SearchIcon from "./../../Assets/search-icon.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="HeaderWrapper">
+      <div className="HeaderLogoWrapper">
+        <img src={Logo} alt="Accent logo" className="HeaderLogo" />
+      </div>
+      <ul className="HeaderNavbar">
+        <li className="HeaderNavbarListItem1">Home</li>
+        <li className="HeaderNavbarListItem">Shop</li>
+        <li className="HeaderNavbarListItem">About</li>
+        <li className="HeaderNavbarListItem">Blog</li>
+        <li className="HeaderNavbarListItem">Contact</li>
+      </ul>
+      <div className="HeaderSearchWarpper">
+        <input type="text" className="HeaderSearchInput"></input>
+        <img src={SearchIcon} alt="search-icon" className="HeaderSearchIcon" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
