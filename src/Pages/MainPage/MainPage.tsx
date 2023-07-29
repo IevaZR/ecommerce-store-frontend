46c96c9048f6f8e6320d8c4af50dfbb016aec4e0
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-07-30 01:12:05 +0300

    header section

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import Header from "../../Components/Header/Header";
+import HeroSection from "../../Components/HeroSection/HeroSection";
 import "./MainPage.css";
 
 const MainPage = () => {
   return (
     <div className="MainPageWrapper">
       <Header />
+      <HeroSection/>
     </div>
   );
 };
