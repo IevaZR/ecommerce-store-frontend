3a79058e22a055ab176cdcc4ecad1e87a764fd10
KristianaTR <kristiana.tatarcuka@gmail.com>
2023-07-31 12:28:18 +0300

    create ProductList and ProductCard components dynamic

--- src/Components/ProductCard/ProductCard.tsx
+++ src/Components/ProductCard/ProductCard.tsx
@@ -1,21 +1,28 @@
 import './ProductCard.css';
+import {Furniture} from '../../types/types';
+// import Button from '../ReusableComponents/Button/Button';
+interface ProductCardProps {
+    productList: Furniture;
+}
 
-const ProductCard = () => {
-  return (
+const ProductCard = ({productList}: ProductCardProps) => (
     <div className='ProductCardWrapper'>
         <div className="ProductImageContainer">
-            <img 
-                src="https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw9f9383a1/images/2050000/2052276.jpg?sw=1200" 
-                alt="product img" 
-            />
+            <img
+                src={productList.image}
+                alt="product img" />
         </div>
         <div className="ProductDataContainer">
-            <div className="ProductName">Stylish sofa</div>
-            <div className="ProductPrice">650 EUR</div>
+            <div className="ProductName">
+                {productList.title}
+            </div>
+            <div className="ProductPrice">
+                {productList.price} EUR
+            </div>
         </div>
+        {/* <Button text='View' /> */}
         <button className='ProductCardQuickviewButton'>Quickview</button>
     </div>
-  )
-}
+)
 
 export default ProductCard
