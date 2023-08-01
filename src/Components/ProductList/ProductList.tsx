0c65d274e09a15acdf760cb89805754938e33c1f
lindalejiete <116344082+lindalejiete@users.noreply.github.com>
2023-08-02 00:02:52 +0300

    test filter added to ProductList

--- src/Components/ProductList/ProductList.tsx
+++ src/Components/ProductList/ProductList.tsx
@@ -7,6 +7,8 @@ const ProductList = ({ searchQuery }) => {
     const [products, setProducts] = useState(FurnitureData);
     // console.log(products);
     const [visibleProducts, setVisibleProducts] = useState(8);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    //Line 10 for filter
 
 
     //---FOR SEARCH BY SEARCH QUERY------
@@ -48,6 +50,24 @@ const ProductList = ({ searchQuery }) => {
         setProducts(filteredProducts);
     }, [filteredProducts]);
     //--------END OF SEARCH BY SEARCH QUERY----------
+    
+    //--------START OF FILTER------------------------
+    const filterByCategory = (data, category) => {
+        if (!category) {
+          return data;
+        }
+        return data.filter((item) => item.category.toLowerCase() === category);
+      };
+    
+      useEffect(() => {
+        const filteredProductsByCategory = filterByCategory(FurnitureData, selectedCategory);
+        setProducts(filterBySearchQuery(filteredProductsByCategory, searchQuery));
+      }, [selectedCategory, searchQuery]);
+    
+      const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+      };
+    //--------END OF FILTER--------------------------
 
     const handleLoadMore = () => {
         setVisibleProducts((prevVisibleProducts) => prevVisibleProducts + 8);
@@ -55,19 +75,30 @@ const ProductList = ({ searchQuery }) => {
 
     const productsToShow = products.slice(0, visibleProducts);
 
+
+
     return (
-        <div className="ProductListWrapper">
-            <div className="ProductListContent">
-                {productsToShow.map((product) => (
-                    <ProductCard key={product.id} productList={product} />
-                ))}
+        <>
+            <ul>
+                <li onClick={() => handleCategoryClick("sofas")}>Sofas</li>
+                <li onClick={() => handleCategoryClick("chairs")}>Chairs</li>
+                <li onClick={() => handleCategoryClick("tables")}>Tables</li>
+                <li onClick={() => handleCategoryClick("beds")}>Beds</li>
+            </ul>
+
+            <div className="ProductListWrapper">
+                <div className="ProductListContent">
+                    {productsToShow.map((product) => (
+                        <ProductCard key={product.id} productList={product} />
+                    ))}
+                </div>
+                {visibleProducts < products.length && (
+                    <button onClick={handleLoadMore} className="LoadMoreButton">
+                        Load More
+                    </button>
+                )}
             </div>
-            {visibleProducts < products.length && (
-                <button onClick={handleLoadMore} className="LoadMoreButton">
-                    Load More
-                </button>
-            )}
-        </div>
+        </>
     );
 };
 
