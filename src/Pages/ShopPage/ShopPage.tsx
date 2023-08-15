bb2bdd4658495cea742bbc299bf90d4905628e50
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-08-15 13:34:22 +0300

    product view moved to shop page

--- src/Pages/ShopPage/ShopPage.tsx
+++ src/Pages/ShopPage/ShopPage.tsx
@@ -1,20 +1,15 @@
 import Header from "../../Components/Header/Header";
 import "./ShopPage.css";
-import { useActiveSearchContext } from "../../HelperFunctions/ActiveSearchContext";
-import ProductList from "../../Components/ProductList/ProductList";
 import { FilterProvider } from "../../HelperFunctions/FilterContext";
 import Filter from "../../Components/Filter/Filter";
 import ProductsView from "../../Components/ProductsView/ProductsView";
 
 const ShopPage = () => {
-  const { activeSearch, searchQuery, showAllProducts } =
-    useActiveSearchContext();
-
   return (
     <FilterProvider>
       <div id="ShopPage" className="ShopPageWrapper">
         <Header />
-        <Filter/>
+        {/* <Filter/> */}
         <ProductsView/>
       </div>
     </FilterProvider>
