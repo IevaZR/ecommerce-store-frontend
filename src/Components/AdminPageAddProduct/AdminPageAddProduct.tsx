f4850f1ef9ed0c3d7cb04b1687d9f9de5b4bb3b2
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-08-16 17:42:30 +0300

    work on add product form

--- src/Components/AdminPageAddProduct/AdminPageAddProduct.tsx
+++ src/Components/AdminPageAddProduct/AdminPageAddProduct.tsx
@@ -1,35 +1,61 @@
 import React from "react";
 import "./AdminPageAddProduct.css";
+import Button from "../ReusableComponents/Button/Button";
 
 const AdminPageAddProduct = () => {
   return (
     <div className="AdminPageAddProductWrapper">
       <div className="AdminPageAddProductFormWrapper">
-        <h3 className="AdminPageAddProductHeading">
-          To add a new product, please fill out the form below
-        </h3>
+        <div className="AdminPageAddProductHeadingWrapper">
+          <h3 className="AdminPageAddProductHeading">
+            Please fill out the form below to add a new product
+          </h3>
+        </div>
+
         <form className="AdminPageAddProductsForm">
           <label className="AdminPageAddProductsLabel">TITLE</label>
-          <input className="AdminPageAddProductsInput" placeholder="TITLE"/>
+          <input className="AdminPageAddProductsInput" placeholder="TITLE" />
           <label className="AdminPageAddProductsLabel">CATEGORY</label>
-          <input className="AdminPageAddProductsInput" placeholder="CATEGORY"/>
+          <input className="AdminPageAddProductsInput" placeholder="CATEGORY" />
           <label className="AdminPageAddProductsLabel">PRICE</label>
-          <input className="AdminPageAddProductsInput" placeholder="PRICE"/>
+          <input className="AdminPageAddProductsInput" placeholder="PRICE" />
           <label className="AdminPageAddProductsLabel">DESCRIPTION</label>
-          <input className="AdminPageAddProductsInput" placeholder="DESCRIPTION"/>
+          <input
+            className="AdminPageAddProductsInput"
+            placeholder="DESCRIPTION"
+          />
           <label className="AdminPageAddProductsLabel">COLOR</label>
-          <input className="AdminPageAddProductsInput" placeholder="COLOR"/>
+          <input className="AdminPageAddProductsInput" placeholder="COLOR" />
           <label className="AdminPageAddProductsLabel">QUANTITY</label>
-          <input className="AdminPageAddProductsInput" placeholder="QUANTITY"/>
+          <input className="AdminPageAddProductsInput" placeholder="QUANTITY" />
           <label className="AdminPageAddProductsLabel">DIMENSIONS CM</label>
-          <input className="AdminPageAddProductsInput" placeholder="DIMENSIONS CM"/>
+          <div className="AdminPageAddProductsDimensionsWrapper">
+            <label className="AdminPageAddProductsLabel">WIDTH</label>
+            <input
+              className="AdminPageAddProductsInput AdminPageAddProductsInputDimensions"
+              placeholder="DIMENSIONS CM"
+            />
+            <label className="AdminPageAddProductsLabel">HEIGHT</label>
+            <input
+              className="AdminPageAddProductsInput AdminPageAddProductsInputDimensions"
+              placeholder="DIMENSIONS CM"
+            />
+            <label className="AdminPageAddProductsLabel">DEPTH</label>
+            <input
+              className="AdminPageAddProductsInput AdminPageAddProductsInputDimensions"
+              placeholder="DIMENSIONS CM"
+            />
+          </div>
+
           <label className="AdminPageAddProductsLabel">FEATURES</label>
-          <input className="AdminPageAddProductsInput" placeholder="FEATURES"/>
+          <input className="AdminPageAddProductsInput" placeholder="FEATURES" />
           <label className="AdminPageAddProductsLabel">KEYWORDS</label>
-          <input className="AdminPageAddProductsInput" placeholder="KEYWORDS"/>
+          <input className="AdminPageAddProductsInput" placeholder="KEYWORDS" />
           <label className="AdminPageAddProductsLabel">IMAGE</label>
-          <input className="AdminPageAddProductsInput" placeholder="IMAGE"/>
-          <button className="AdminPageAssProductsButton">SUBMIT</button>
+          <input className="AdminPageAddProductsInput" placeholder="IMAGE" />
+          <div className="AdminPageAddProductsButtonWrapper">
+            <Button text="ADD" />
+          </div>
         </form>
       </div>
     </div>
