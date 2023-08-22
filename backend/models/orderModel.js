5dd9d39e4d8ed4066e374902961bb23f97c5fbdd
Ieva Zvanitaja Rubine <ieva.zvanitaja.rubine@gmail.com>
2023-08-22 19:37:53 +0300

    order schema and user schema created

---
+++ backend/models/orderModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    orderNumber: {
+        type: Number,
+        required: true
+    },
+    orderedProducts: [
+        {
+            product: {
+                name: {
+                    type: String,
+                    required
+                },
+                quantity: {
+                    type: Number,
+                    required: true
+                },
+                price: {
+                    type: Number,
+                    required: true
+                }
+            }
+        }
+    ],
+    customer: {
+        type: Object,
+        required:true
+    },
+    orderStatus: {
+        type: String,
+        status: ['Active', 'Fulfilled'],
+        default: 'Active'
+    },
+});
+
+export default mongoose.model("order", orderSchema);
---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true
+    },
+    orders: [orderSchema],
+    favourites: [productSchema]
+})
+
+export default mongoose.model("user", userSchema);
