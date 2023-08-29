c3dc608b7daf155636fff34d4497c24a411bae9e
lindalejiete <116344082+lindalejiete@users.noreply.github.com>
2023-08-29 10:40:33 +0300

    Featured Product component created

--- frontend/src/Components/Advertisment/Advertisement.tsx
+++ frontend/src/Components/Advertisment/Advertisement.tsx
@@ -86,6 +86,7 @@ const Advertisement = () => {
 
   return (
     randomProduct && (
+      <div className="AdvertisementSectionBlock">
       <div
         className={`AdvertisementSectionWrapper ${slideIn ? "slideIn" : ""}`}
       >
@@ -113,6 +114,7 @@ const Advertisement = () => {
             <Button text="Shop Now" onClick={showAllProducts}></Button>
           </Link>
         </div>
+      </div>
       </div>
     )
   );
--- frontend/src/Components/FeaturedProducts/FeaturedProducts.tsx
+++ frontend/src/Components/FeaturedProducts/FeaturedProducts.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import './FeaturedProducts.css'
-import ProductCard from '../ProductCard/ProductCard'
+import React, { useState, useEffect } from 'react';
+import './FeaturedProducts.css';
+import ProductCard from '../ProductCard/ProductCard';
+import axios from 'axios';
 
 const FeaturedProducts = () => {
-  return (
-    <div>
-        FeaturedProducts
-    </div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const [randomProducts, setRandomProducts] = useState([]);
+  const [visibleProducts, setVisibleProducts] = useState(4);
 
-export default FeaturedProducts
+  useEffect(() => {
+    const handleFetch = async () => {
+      try {
+        const response = await axios.get('http://localhost:3009/get-all');
+        setProducts(response.data);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+      }
+    };
+
+    handleFetch();
+  }, []);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      getRandomProducts();
+    }
+  }, [products]);
+
+  const getRandomProducts = () => {
+    const shuffledProducts = [...products].sort(() => Math.random() - 0.5);
+    const randomProductsSlice = shuffledProducts.slice(0, visibleProducts);
+    setRandomProducts(randomProductsSlice);
+  };
+
+  return (
+    <div className="FeaturedProductListContentWrapper">
+      <h2>Featured Products</h2>
+    <div className="FeaturedProductListContent">
+      {randomProducts.map((product) => (
+        <ProductCard key={product.id} productList={product} />
+      ))}
+    </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
