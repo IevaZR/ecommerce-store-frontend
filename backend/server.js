0d2922b2f96f91b5f30ebbdc1579e566dce15d52
IevaZR <ieva.zvanitaja.rubine@gmail.com>
2023-12-01 12:47:14 +0200

    backend for deploy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 import cors from 'cors'
 
 const app = express();
-const port = 3009;
+const port = process.env.PORT || 3009;
 
 app.use(express.json())
 app.use(cors())
